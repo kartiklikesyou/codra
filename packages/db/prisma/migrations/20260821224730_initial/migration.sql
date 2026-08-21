be433b9fd773885a59446784fc526eddf8e2afd3
kartiklikesyou <kartikkamal100@gmail.com>
2026-08-22 04:20:13 +0530

    new beginning

--- packages/db/prisma/migrations/20260821224730_initial/migration.sql
+++ packages/db/prisma/migrations/20260821224730_initial/migration.sql
@@ -7,15 +7,5 @@ CREATE TABLE "User" (
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
--- CreateTable
-CREATE TABLE "Project" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
-);
-
 -- CreateIndex
 CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
